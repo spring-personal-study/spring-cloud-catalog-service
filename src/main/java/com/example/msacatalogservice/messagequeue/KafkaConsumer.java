42cc9a9c1866e8ca63b7f234cacd32565ed4edc4
chaenabi <find1086@gmail.com>
2022-04-04 07:38:01 +0900

    topic change

--- src/main/java/com/example/msacatalogservice/messagequeue/KafkaConsumer.java
+++ src/main/java/com/example/msacatalogservice/messagequeue/KafkaConsumer.java
@@ -11,6 +11,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 // 리스너를 통해 데이터를 가져오고 그 데이터로 DB에 있는 자료를 업데이트하는 목적으로 사용
@@ -20,7 +21,7 @@ import java.util.Map;
 public class KafkaConsumer {
     private final CatalogRepository repository;
 
-    @KafkaListener(topics = "example-catalog-topic")
+    @KafkaListener(topics = {"example-catalog-topic"})
     public void updateQty(String kafkaMessage) {
       log.info("Kafka Message: ->" + kafkaMessage);
 
@@ -33,12 +34,36 @@ public class KafkaConsumer {
             e.printStackTrace();
         }
 
-        String productId = (String) map.get("productId");
+        /*
+        {"schema":{
+                "type":"struct",
+                "fields":[{
+                            "type":"string","optional":true,"field":"order_id"},
+                            {"type":"string","optional":true,"field":"user_id"},
+                            {"type":"string","optional":true,"field":"product_id"},
+                            {"type":"int32","optional":true,"field":"qty"},
+                            {"type":"int32","optional":true,"field":"unit_price"},
+                            {"type":"int32","optional":true,"field":"total_price"
+                          }],
+                "optional":false,
+                "name":"orders"
+                },
+          "payload":{"order_id":"99853df1-603b-4f49-bea3-c17676b60add",
+                    "user_id":null,
+                    "product_id":"CATALOG-001",
+                    "qty":1,
+                    "unit_price":150,
+                    "total_price":150}
+         }
+         */
+
+        //String productId = (String) map.get("productId");
+        String productId = (String) ((LinkedHashMap<String, Object>) map.get("payload")).get("product_id");
         CatalogEntity entity = repository.findByProductId(productId);
 
         // 수량 변경
         if (entity != null) {
-            entity.updateStock(entity.getStock() - (Integer) map.get("qty"));
+            entity.updateStock(entity.getStock() - (Integer) ((LinkedHashMap<String, Object>) map.get("payload")).get("qty"));
             repository.save(entity);
         }
 
